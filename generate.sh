02802dcdec43519e7672bedbb4ffd7902c0af88d
charles francis larrieu casias <chuck@charless-MacBook-Pro.local>
2025-03-14 14:20:50 -0700

    wip

--- generate.sh
+++ generate.sh
@@ -19,81 +19,113 @@ echo "Create our own Root CA"
 openssl req \
     -x509 -sha256 -days 1825 -newkey rsa:2048 \
     -keyout ${CA_PATH}/rootCA.key -out ${CA_PATH}/rootCA.crt \
-    -passout pass:changeit \
-    -subj '/CN=ca1.test.conduktor.io/OU=TEST/O=CONDUKTOR/L=LONDON/C=UK'
+    -passout pass:conduktor \
+    -subj '/CN=rootCA/OU=TEST/O=CONDUKTOR/L=LONDON/C=UK'
 }
 
+
+
+
 clean_certificates() {
-    # Cleanup files
     local DIR="${CA_PATH}"
     echo "Cleaning up directory $DIR"
-    (cd "${DIR}" && rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log)
-    (cd "${DIR}" && rm -fr keypair/)
+    (cd "${DIR}" && rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log *.ext)
 }
+
+
+
 
 create_certificate() {
 
-# Get subject alternate names from arguments
-declare -a subject_alt_names
-for i in "$@"
+# Get subject alternate names (SANs) from arguments
+declare subject_alt_names
+i=0
+for san in "$@"
 do
-    subject_alt_names+=($i)
+    subject_alt_names+=",DNS.$i:${san}"
+    ((i++))
 done
+# get rid of leading comma
+subject_alt_names=${subject_alt_names:1}
 
 echo
-echo "Generate Private Key"
-openssl genrsa -des3 -passout pass:changeit -out $1.key 2048
+echo "Generating ext file for SANs"
+echo "$subject_alt_names"
 
-echo
-echo "Generate certificate signing request - This would have been sent to the CA"
-openssl req \
-    -key $1.key \
-    -new -out $1.csr \
-    -passin pass:changeit \
-    -subj "/CN=${1}/OU=TEST/O=CONDUKTOR/L=LONDON/C=UK"
-
-cat <<END > $1.san.ext
+cat <<END > ${CA_PATH}/$1.san.ext
 [req]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_req
 prompt = no
 [req_distinguished_name]
-CN = $user
+CN = $1
 [v3_req]
 extendedKeyUsage = serverAuth, clientAuth
-subjectAltName = $san_for_config
+subjectAltName = $subject_alt_names
 END
+
+
+echo
+echo "Generate Private Key"
+openssl genrsa -des3 -passout pass:conduktor -out ${CA_PATH}/$1.key 2048
+
+echo
+echo "Generate certificate signing request - This would have been sent to the CA"
+openssl req \
+    -key ${CA_PATH}/$1.key \
+    -new -out ${CA_PATH}/$1.csr \
+    -passin pass:conduktor \
+    -subj "/CN=${1}/OU=TEST/O=CONDUKTOR/L=LONDON/C=UK" \
+    -config ${CA_PATH}/$1.san.ext
+
 
 echo
 echo "Generate Certificate, signed by Root CA with SAN"
-openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in domain.csr -out domain.crt -days 365 -CAcreateserial -extfile san-extension -passin pass:changeit
+openssl x509 -req \
+    -CA ${CA_PATH}/rootCA.crt -CAkey ${CA_PATH}/rootCA.key \
+    -in ${CA_PATH}/$1.csr \
+    -out ${CA_PATH}/$1.crt \
+    -days 365 -CAcreateserial \
+    -extfile ${CA_PATH}/$1.san.ext \
+    -passin pass:conduktor
 
 echo
 echo "Show the content of the signed Certificate"
-openssl x509 -text -noout -in domain.crt -passin pass:changeit
+openssl x509 -text -noout -in ${CA_PATH}/$1.crt -passin pass:conduktor
 
 echo
-echo Generate a PKCS12 Keystore with alias 'domainalias'
-openssl pkcs12 -inkey domain.key -in domain.crt -export -out domain.p12 -passin pass:changeit -passout pass:changeit -name domainalias
+echo "Creating full certificate chain"
+cat ${CA_PATH}/$1.crt ${CA_PATH}/rootCA.crt > ${CA_PATH}/$1.fullchain.crt
+
+echo
+echo "Generate a PKCS12 Keystore with alias $1"
+openssl pkcs12 -inkey ${CA_PATH}/$1.key -in ${CA_PATH}/$1.fullchain.crt -export -out ${CA_PATH}/$1.p12 -passin pass:conduktor -passout pass:conduktor -name $1
 
 echo
 echo "Show the content of the PKCS12 Keystore from the point of view of Java keytool, to see the alias"
-keytool -list -v -keystore domain.p12 -storepass changeit -storetype PKCS12
+keytool -list -v -keystore ${CA_PATH}/$1.p12 -storepass conduktor -storetype PKCS12
 
 echo
-echo "Generate a JKS Keystore from PKCS12 Keystore (note the alias '1')"
-keytool -importkeystore -deststorepass changeit -destkeypass changeit -destkeystore domain.keystore.jks -deststoretype JKS -srckeystore domain.p12 -srcstoretype PKCS12 -srcstorepass changeit -alias domainalias
+echo "Generate a JKS Keystore from PKCS12 Keystore"
+keytool \
+    -importkeystore \
+    -deststorepass conduktor -destkeypass conduktor \
+    -destkeystore ${CA_PATH}/$1.keystore.jks \
+    -deststoretype PKCS12 -srckeystore ${CA_PATH}/$1.p12 \
+    -srcstoretype PKCS12 \
+    -srcstorepass conduktor \
+    -alias $1
 
 echo
 echo "Show the content of the JKS Keystore"
-keytool -list -v -keystore domain.keystore.jks -storepass changeit
+keytool -list -v -keystore ${CA_PATH}/$1.keystore.jks -storepass conduktor
 
 }
 
 create_truststore() {
 keytool -noprompt \
     -keystore ${CA_PATH}/truststore.jks \
-    -alias snakeoil-caroot \
+    -alias rootCA \
     -import -file ${CA_PATH}/rootCA.crt \
     -storepass conduktor \
     -keypass conduktor
@@ -105,4 +137,5 @@ keytool -noprompt \
 clean_certificates
 create_ca
 create_truststore
-echo hello
+create_certificate kafka franz-kafka.conduktor.svc.cluster.local *.franz-kafka-controller-headless.conduktor.svc.cluster.local
+create_certificate gateway.conduktor.k8s.orb.local brokermain0-gateway.conduktor.k8s.orb.local brokermain1-gateway.conduktor.k8s.orb.local brokermain2-gateway.conduktor.k8s.orb.local
