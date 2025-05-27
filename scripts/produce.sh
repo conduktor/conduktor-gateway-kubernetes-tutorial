cbb80ac515f345887918e622f269b3034deb94f3
charles francis larrieu casias <chuck@charless-MacBook-Pro.local>
2025-05-27 14:26:13 -0700

    wip

--- scripts/produce.sh
+++ scripts/produce.sh
@@ -4,7 +4,7 @@ export KAFKA_OPTS="-Djava.security.manager=allow"
 
 kafka-topics --create --topic test \
     --if-not-exists \
-    --bootstrap-server gateway.conduktor.k8s.orb.local:9092 \
+    --bootstrap-server gateway.k8s.tutorial:9092 \
     --command-config $PWD/client.properties
 
 kafka-producer-perf-test \
@@ -12,5 +12,5 @@ kafka-producer-perf-test \
     --throughput 10 \
     --record-size 100 \
     --num-records 10000000 \
-    --producer-props bootstrap.servers=gateway.conduktor.k8s.orb.local:9092 \
+    --producer-props bootstrap.servers=gateway.k8s.tutorial:9092 \
     --producer.config $PWD/client.properties
